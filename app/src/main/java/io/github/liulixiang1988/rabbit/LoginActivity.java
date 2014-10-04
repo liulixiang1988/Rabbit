e0b16e9b0b4139319e66a020fc9b07b718460ade
Liu Lixiang <liulixiang1988@gmail.com>
2014-10-04 12:09:36 +0800

    login to signup

--- app/src/main/java/io/github/liulixiang1988/rabbit/LoginActivity.java
+++ app/src/main/java/io/github/liulixiang1988/rabbit/LoginActivity.java
@@ -1,17 +1,31 @@
 package io.github.liulixiang1988.rabbit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class LoginActivity extends Activity {
+
+    protected TextView txtSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtSignup = (TextView) findViewById(R.id.txtSignUp);
+        txtSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 
