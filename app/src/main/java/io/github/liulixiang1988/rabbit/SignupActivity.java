00aa104a9f63b14c531c176fa5cc421ab3f1fbf0
Liu Lixiang <liulixiang1988@gmail.com>
2014-10-05 11:29:22 +0800

    handle user signup with parse.com

--- app/src/main/java/io/github/liulixiang1988/rabbit/SignupActivity.java
+++ app/src/main/java/io/github/liulixiang1988/rabbit/SignupActivity.java
@@ -2,12 +2,17 @@ package io.github.liulixiang1988.rabbit;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
 public class SignUpActivity extends Activity {
@@ -41,7 +46,29 @@ public class SignUpActivity extends Activity {
                     alertDialog.show();
                 }
                 else{
-                    //todo: new user here
+                    ParseUser user = new ParseUser();
+                    user.setUsername(username);
+                    user.setPassword(password);
+                    user.setEmail(email);
+
+                    user.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null){
+                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                                builder.setTitle(R.string.signup_error_title)
+                                        .setMessage(e.getMessage())
+                                        .setPositiveButton(android.R.string.ok, null);
+                                AlertDialog alertDialog = builder.create();
+                                alertDialog.show();
+                            }
+                        }
+                    });
                 }
             }
         });
